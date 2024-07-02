72fd04c46cf4704f1e371dc8895f49fb16d71201
João Oliveira <joao.oliveira0117@hotmail.com>
2024-07-02 13:32:02 -0300

    serve basic html route

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,17 @@ import health from './health/index.js';
 import user from './user/index.js';
 import occurrence from './occurrence/index.js';
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const router = Router();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+router.get('/sinoslog', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', '..', 'http', 'index.html'));
+});
 
 router.use('/auth', auth);
 router.use('/health', health);
